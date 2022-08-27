b1bc9bd14b21f2cb7b6226ecbcb53c92f265160d
Harry <harry515952831@gmail.com>
2022-08-28 02:30:34 +1000

    showlist

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -3,50 +3,67 @@ export const ContactItem = (props) => {
   const [isEdit, setIsEdit] = useState(false);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState(0);
+  const [open, setOpen] = useState(false);
 
   const { contactElement, deleteItem, editItem } = props;
   return (
-    <li key={contactElement.id}>
-      <div className="user-left">
-        <div className="user-name-letter">
-          {contactElement.name.charAt(0).toUpperCase()}
-        </div>
-        <div className="user-name">{contactElement.name}</div>
-        {isEdit && (
-          <input
-            className="edit-input"
-            type="text"
-            placeholder={contactElement.name}
-            onChange={(e) => setNewName(e.target.value)}
-          />
-        )}
-        {isEdit && (
-          <input
-            className="edit-input"
-            type="number"
-            placeholder={contactElement.phone}
-            onChange={(e) => setNewPhone(e.target.value)}
-          />
-        )}
-        {isEdit && (
-          <button
+    <>
+      <li key={contactElement.id}>
+        <div className="user-left">
+          <div
+            className="user-name-letter"
             onClick={() => {
-              editItem(newName, newPhone, contactElement.id);
-              setIsEdit(false);
+              setOpen(!open);
             }}
           >
-            save
-          </button>
-        )}
-      </div>
-      <div className="user-right">
-        <div className="user-edit">
-          <button onClick={() => setIsEdit(!isEdit)}>edit</button>
+            {contactElement.name.charAt(0).toUpperCase()}
+          </div>
+          <div className="user-name">{contactElement.name}</div>
+          {isEdit && (
+            <input
+              className="edit-input"
+              type="text"
+              placeholder={contactElement.name}
+              onChange={(e) => setNewName(e.target.value)}
+            />
+          )}
+          {isEdit && (
+            <input
+              className="edit-input"
+              type="number"
+              placeholder={contactElement.phone}
+              onChange={(e) => setNewPhone(e.target.value)}
+            />
+          )}
+          {isEdit && (
+            <button
+              onClick={() => {
+                editItem(newName, newPhone, contactElement.id);
+                setIsEdit(false);
+              }}
+            >
+              save
+            </button>
+          )}
         </div>
-        <div className="user-delete">
-          <button onClick={() => deleteItem(contactElement.id)}>delete</button>
+        <div className="user-right">
+          <div className="user-edit">
+            <button onClick={() => setIsEdit(!isEdit)}>edit</button>
+          </div>
+          <div className="user-delete">
+            <button onClick={() => deleteItem(contactElement.id)}>
+              delete
+            </button>
+          </div>
         </div>
-      </div>
-    </li>
+      </li>
+      {/* {open&&<div>Name:{contactElement.name},Phone:{contactElement.phone}<span onClick={()=>setOpen(false)}>x</span></div>} */}
+      {open && (
+        <div className="showlist" onClick={() => setOpen(false)}>
+          <span className="showName">Name:{contactElement.name}</span>
+          <span className="showPhone">Phone:{contactElement.phone}</span>
+        </div>
+      )}
+    </>
   );
 };
